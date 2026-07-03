4d3ae2d8aa5f06dd3141c2cb59e5678ac1b7f8c3
ABHINANDA-V <abhinandav96@gmail.com>
2026-07-03 23:33:35 +0530

    is and in operators

--- example1.py
+++ example1.py
@@ -1,5 +1,21 @@
-age=int(input("Enter the age:"))
-if age>=18:
-    print("You are adult")
-else:
-    print("You are minor")    
+# age=int(input("Enter the age:"))
+# if age>=18:
+#     print("You are adult")
+# else:
+#     print("You are minor")    
+
+#===========================================
+# is operator(identity operator)
+# x=[1,2,3,4,5]
+# y=[1,2,3,4,5]
+# z=x
+# print(x is y)
+# print(x is z)
+
+#==============================================
+# in operator(membership operator)
+nums = [1,2,3,4,5]
+print(4 in nums)
+print(12 in nums)
+str1= "Python"
+print("y" in str1)
